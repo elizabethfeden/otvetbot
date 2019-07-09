43b6d9b601cc84b81dd7732fb245f9c571a0a1a3
elizabethfeden <elizabethfeden@gmail.com>
2019-07-09 11:10:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -678,6 +678,7 @@ def main():
 	dp.add_handler(myquestion_handler)
 	dp.add_handler(answerhandler)
 	dp.add_handler(messagemehandler)
+	dp.add_handler(settitlehandler)
 	dp.add_handler(MessageHandler(Filters.all, default))
 
 	dp.add_error_handler(error)
